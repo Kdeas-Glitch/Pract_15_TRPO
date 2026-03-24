bba0fa0888ac9b6cdabf25c6c58eac0897f69076
bakaqqw <iliaudilov@gmail.com>
2026-03-24 13:49:48 +0500

    Sdelal

---
+++ Pract_15_TRPO/ValidationRules/IsNumberValidMoreThan0 .cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Pract_15_TRPO.ValidationRules
+{
+    public class IsNumberValidMoreThan0 : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var input = (value ?? "").ToString().Trim();
+            if (!float.TryParse(input, out float floatValue))
+            {
+                return new ValidationResult(false, "Необходимо ввести число");
+            }
+            if(Convert.ToInt32(input)>0)
+            return ValidationResult.ValidResult;
+            else
+            {
+                return new ValidationResult(false, "Необходимо ввести число Больше 0");
+            }
+        }
+    }
+}
