faba422fe8c1d3d85bf929db18a37922d4bdc3ef
bakaqqw <iliaudilov@gmail.com>
2026-03-28 15:02:55 +0500

    Final

---
+++ Pract_15_TRPO/ValidationRules/IsNumberValidMore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Pract_15_TRPO.ValidationRules
+{
+    class IsNumberValidMore : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var input = (value ?? "").ToString().Trim();
+            input = input.Replace('.', ',');
+            if (!double.TryParse(input, out double floatValue))
+            {
+                return new ValidationResult(false, "Необходимо ввести число");
+            }
+            if (Convert.ToDouble(input) >0)
+                return ValidationResult.ValidResult;
+            else
+            {
+                return new ValidationResult(false, "Необходимо ввести число Больше 0");
+            }
+        }
+    }
+}
